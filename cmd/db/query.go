f4f539492635f1840435e29fb24a640800fac93d
Bruno Brolesi <brunohsbrolesi@gmail.com>
2024-02-14 00:19:21 -0300

    fix: migration queries

--- cmd/db/query.go
+++ cmd/db/query.go
@@ -1,5 +1,5 @@
 package db
 
 const (
-	CREATE_KEYSPACE = "CREATE KEYSPACE rinha WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'replication_factor' : 1  } AND DURABLE_WRITES = true;"
+	CREATE_KEYSPACE = "CREATE KEYSPACE IF NOT EXISTS rinha WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'replication_factor' : 1  } AND DURABLE_WRITES = true;"
 )

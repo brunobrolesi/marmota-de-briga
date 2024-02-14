da02bcd2b5b35bc19fb7d9da9b5cfd2101966487
Bruno Brolesi <brunohsbrolesi@gmail.com>
2024-02-14 00:18:11 -0300

    feat: add retry in scylla connection

--- internal/infrastructure/repository/config/scylla_client.go
+++ internal/infrastructure/repository/config/scylla_client.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"log"
+	"time"
+
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
 )
@@ -8,11 +11,17 @@ import (
 const KEYSPACE = "rinha"
 
 func NewScyllaClient(keyspace string) *gocqlx.Session {
-	cluster := gocql.NewCluster("scylla-db")
-	cluster.Keyspace = keyspace
-	session, err := gocqlx.WrapSession(cluster.CreateSession())
-	if err != nil {
-		panic(err)
+	var err error
+	var session gocqlx.Session
+	for ok := true; ok; ok = err != nil {
+		cluster := gocql.NewCluster("scylla-db")
+		cluster.Keyspace = keyspace
+		session, err = gocqlx.WrapSession(cluster.CreateSession())
+		if err != nil {
+			log.Println(err)
+			time.Sleep(5 * time.Second)
+		}
 	}
 	return &session
+
 }
